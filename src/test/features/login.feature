7606eb7fa861b281ba0ed20278753974e0f5c770
jennymarin1989 <jennymarin1989@hotmail.comm>
2025-01-26 20:55:49 +0100

    Added test for loginpage in saucedemo - need to remove timeouts

--- src/test/features/login.feature
+++ src/test/features/login.feature
@@ -1,7 +1,11 @@
 Feature: Login to Saucedemo
 
+    Background: visit the page
+      Given I visit "https://saucedemo.com"
+
     Scenario: User can successfully log in 
-        Given I visit the page "https://saucedemo.com"
+        Given The LoginPage is visible
+        Then I click on the "login-button"
     
      
         
--- src/test/step-definitions/loginStep.ts
+++ src/test/step-definitions/loginStep.ts
@@ -1,13 +1,28 @@
-import { Before, Given } from '@cucumber/cucumber';
+import { Before, Given, Then, After, AfterAll } from '@cucumber/cucumber';
 import { Builder, WebDriver } from 'selenium-webdriver';
 
 let driver: WebDriver;
 
 Before(async () => {
-  driver = await new Builder().forBrowser('chrome').build(); //create a builder instance for chrome browser
+  driver = await new Builder().forBrowser('chrome').build();
 });
 
-Given('I visit the page {string}', async function (homepage: string) {
-  console.log('user can navigate', homepage);
+Given('I visit {string}', { timeout: 25000 }, async (homepage: string) => {
   await driver.get(homepage);
 });
+
+Given('The LoginPage is visible', { timeout: 15000 }, async () => {
+  await driver.get('https://saucedemo.com');
+  await driver.findElement({ css: '[data-test="login-container"]' }); //find container
+  await driver.sleep(5000);
+});
+
+Then('I click on the {string}', { timeout: 15000 }, async (buttonElement: string) => {
+  await driver.get('https://saucedemo.com');
+
+  const element = await driver.findElement({ css: `[data-test=${buttonElement}]` }); //find button element and click
+  await element.click();
+  await driver.sleep(5000);
+});
+
+AfterAll(async () => await driver.quit());
